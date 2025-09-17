73698fb4d88688edbe1ccd6471277b8b6dfbc0b9
Kenji-08 <123200723+Kenji-08@users.noreply.github.com>
2025-09-17 09:40:41 -0600

    prep 1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter the grade percentage: ");
+        int PERCENTAGE = int.Parse(Console.ReadLine());
+        string letterGrade = getLetterGrade(PERCENTAGE);
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+        if (letterGrade == "D" || letterGrade == "F")
+        { Console.WriteLine($"You got a 70 or lower this means you fail."); }
+        
+    }
+
+    static string getLetterGrade(int percentage)
+    {
+        string letter = "";
+        if (percentage >= 90)
+        { letter = "A"; }
+        else if (percentage >= 80)
+        { letter = "B"; }
+        else if (percentage >= 70)
+        { letter = "C"; }
+        else if (percentage >= 60)
+        { letter = "D"; }
+        else{ letter = "F"; }
+        return letter;
     }
 }
