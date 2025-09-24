66881a726a07a636c821f576f4ecb68ecfcb14df
Kenji-08 <123200723+Kenji-08@users.noreply.github.com>
2025-09-24 09:13:41 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+
+        Console.Write("What is the magic number? ");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Number is too high");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Number is too low");
+            }
+        } while (guess != magicNumber);
+        Console.WriteLine("You guessed it!");
+
     }
 }
