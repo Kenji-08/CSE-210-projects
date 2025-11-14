898b5f8731a03b6f406ef4d3f50632fca73d939b
Kenji-08 <123200723+Kenji-08@users.noreply.github.com>
2025-11-13 22:37:00 -0700

    polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Authentication;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 3);
+        Rectangle rect = new Rectangle("red", 8, 5);
+        Circle circle = new Circle("green", 7);
+        shapes.Add(square);
+        shapes.Add(rect);
+        shapes.Add(circle);
+
+        for (int i = 0; i < shapes.Count; i++)
+        {
+            Console.WriteLine($"Shape: {shapes[i].GetType()}\n\tColor: {shapes[i].GetColor()}\n\tArea: {shapes[i].GetArea()}");
+        }
     }
 }
